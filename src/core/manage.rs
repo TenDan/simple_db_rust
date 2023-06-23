2660e83ad1d0839f4953a25df4413e8fef17f64e
Maciej Ślusarz <mslusarz@student.agh.edu.pl>
2023-06-23 12:07:16 +0200

    Add implementation for Database struct

--- src/core/manage.rs
+++ src/core/manage.rs
@@ -1,17 +1,33 @@
+use super::error::ErrorMessage;
 
+type StringOrErrorMessage = Result<String, ErrorMessage>;
 
 enum EntryType {
     Database,
     Entity,
 }
 
-struct Database {
+pub struct Database {
 
 }
 
 // TODO
-//impl BasicOperations for Database {}
-//impl OpenOperation for Database {}
+impl BasicOperations for Database {
+    fn create(&self, args: &str) -> StringOrErrorMessage {
+        Ok(String::from("Created database"))
+    }
+    fn update(&self, args: &str) -> StringOrErrorMessage {
+        Ok(String::from("Updated database"))
+    }
+    fn delete(&self, args: &str) -> StringOrErrorMessage {
+        Ok(String::from("Deleted database"))
+    }
+}
+impl OpenOperation for Database {
+    fn open(&self, args: &str) -> StringOrErrorMessage {
+        Ok(String::from("Opened database"))
+    }
+}
 
 struct Entity {
 
@@ -21,18 +37,18 @@ struct Entity {
 //impl BasicOperations for Entity {}
 //impl ReadOperation for Entity {}
 
-trait BasicOperations {
-    fn create(&self, args: &str);
+pub trait BasicOperations {
+    fn create(&self, args: &str) -> StringOrErrorMessage;
 
-    fn update(&self, args: &str);
+    fn update(&self, args: &str) -> StringOrErrorMessage;
 
-    fn delete(&self, args: &str);
+    fn delete(&self, args: &str) -> StringOrErrorMessage;
 }
 
-trait ReadOperation {
-    fn read(&self, args: &str);
+pub trait ReadOperation {
+    fn read(&self, args: &str) -> StringOrErrorMessage;
 }
 
-trait OpenOperation {
-    fn open(&self, args: &str);
+pub trait OpenOperation {
+    fn open(&self, args: &str) -> StringOrErrorMessage;
 }
